63fd8096e15308d3205ba86827d678caac9b035b
redmairo <redmairo@yahoo.com>
2015-07-25 19:58:47 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -78,7 +78,7 @@
         maximumCycletime: 10,
         voteSkip: false,
         voteSkipLimit: 10,
-        historySkip: false,
+        historySkip: true,
         timeGuard: true,
         maximumSongLength: 10,
         autodisable: true,
