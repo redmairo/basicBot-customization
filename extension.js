704e8399adc7fb8edcb3610129a6c3159a46fef4
redmairo <redmairo@yahoo.com>
2015-07-25 20:35:41 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -67,7 +67,7 @@
         smartSkip: true,
         cmdDeletion: true,
         maximumAfk: 120,
-        afkRemoval: true,
+        afkRemoval: false,
         maximumDc: 60,
         bouncerPlus: true,
         blacklistEnabled: true,
